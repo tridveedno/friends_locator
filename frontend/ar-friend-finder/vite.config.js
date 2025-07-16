bd8570263685177b9dfe12f9852debd90bb86ad2
tridveedno <120334990+tridveedno@users.noreply.github.com>
2025-07-16 10:25:27 +0200

    Update vite.config.js

--- frontend/ar-friend-finder/vite.config.js
+++ frontend/ar-friend-finder/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://c8fe3e3e0653.ngrok-free.app',
-        changeOrigin: true,
-        secure: false,
-      },
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: { outDir: 'dist' },
-});
+})
